82db5a200070c062a2698492a8a8105196e8d1fc
bhtz <heintz.benjamin@gmail.com>
2015-06-07 22:49:27 +0200

    refactoring ionic form

--- src/forms/IonicForm.js
+++ src/forms/IonicForm.js
@@ -8,7 +8,7 @@ var URLS = {
     blank: 'https://github.com/microscope-mobile/microscope-mobile-blank/archive/master.zip',
     sidemenu: 'https://github.com/microscope-mobile/microscope-mobile-sidemenu/archive/master.zip',
     tabs: 'https://github.com/microscope-mobile/microscope-mobile-tabs/archive/master.zip',
-    maps : 'https://github.com/microscope-mobile/microscope-mobile-maps/archive/master.zip'
+    maps: 'https://github.com/microscope-mobile/microscope-mobile-maps/archive/master.zip'
 };
 
 /**
@@ -27,8 +27,7 @@ var IonicForm = Form.extend({
                 { name: 'blank', value: 1 },
                 { name: 'sidemenu', value: 2 },
                 { name: 'tabs', value: 3 },
-                { name: 'maps', value: 4 },
-                { name: 'exit', value: 0 }
+                { name: 'maps', value: 4 }
             ]
         }, {
                 type: 'input',
@@ -44,50 +43,49 @@ var IonicForm = Form.extend({
     },
 
     response: function (answer) {
-        var self = this;
-
         console.log('\n');
+
         var projectPath = path.join(process.cwd(), answer.solution);
+        var name = '';
+        var url = '';
 
         switch (answer.starter) {
             case 1:
-                console.log('... downloading blank starter kit ...');
-                ionicAppLib.utils.fetchArchive(projectPath, URLS.blank).then(function () {
-                    fs.rename(projectPath + '/microscope-mobile-blank-master', projectPath + '/' + answer.project, function () {
-                        console.log('download complete !!');
-                    });
-                });
+                name = 'blank';
+                url = URLS.blank;
                 break;
 
             case 2:
-                console.log('... downloading sidemenu starter kit ...');
-                ionicAppLib.utils.fetchArchive(projectPath, URLS.sidemenu).then(function () {
-                    fs.rename(projectPath + '/microscope-mobile-sidemenu-master', projectPath + '/' + answer.project, function () {
-                        console.log('download complete !!');
-                    });
-                });
+                name = 'sidemenu';
+                url = URLS.sidemenu;
                 break;
 
             case 3:
-                console.log('... downloading tabs starter kit ...');
-                ionicAppLib.utils.fetchArchive(projectPath, URLS.tabs).then(function () {
-                    fs.rename(projectPath + '/microscope-mobile-tabs-master', projectPath + '/' + answer.project, function () {
-                        console.log('download complete !!');
-                    });
-                });
+                name = 'tabs';
+                url = URLS.tabs;
                 break;
-                
+
             case 4:
-                console.log('... downloading maps starter kit ...');
-                ionicAppLib.utils.fetchArchive(projectPath, URLS.maps).then(function () {
-                    fs.rename(projectPath + '/microscope-mobile-maps-master', projectPath + '/' + answer.project, function () {
-                        console.log('download complete !!');
-                    });
-                });
+                name = 'maps';
+                url = URLS.maps;
                 break;
         }
 
+        this.download(name, answer.project, projectPath, url);
+
         console.log('\n');
+    },
+    
+    /**
+     * download zip
+     */
+    download: function (templateName, projectName, projectPath, url) {
+        console.log('... downloading ' + templateName + ' starter kit ...');
+        ionicAppLib.utils.fetchArchive(projectPath, url).then(function () {
+            fs.rename(projectPath + '/microscope-mobile-' + templateName + '-master', projectPath + '/' + projectName, function () {
+                console.log('download complete !!');
+            });
+        });
     }
 });
 
