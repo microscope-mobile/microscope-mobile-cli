376d352acbdbe5c48e2d989e5a50c9b90fdcec88
bhtz <heintz.benjamin@gmail.com>
2015-06-11 23:23:25 +0200

    add commander args parsing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,54 @@
 #!/usr/bin/env node
 
+var program = require('commander');
 var HomeForm = require('./forms/HomeForm');
 
-var logo = [
-	'\n',
-	'=================================\n',
-	'        MICROSCOPE-MOBILE        \n',
-	'                                 \n',
-	'    awesome mobile generator     \n',
-	'================================='
-].join('');
-console.log(logo);
+program
+	.command('ionic <project> <name>')
+	.description('microscope-mobile ionic templates')
+	.action(function(project, name){
+		console.log("\n microscope-mobile ionic not implemented yet");
+	});
+  
+program
+	.command('ionitron <project> <name>')
+	.description('official ionic templates')
+	.action(function(project, name){
+		console.log("\n official ionic not implemented yet");
+	});
+  
+program
+	.command('winjs <project> <name>')
+	.description('WinJS templates')
+	.action(function(project, name){
+		console.log("\n WinJS not implemented yet");
+	});
 
-// run repository form
-new HomeForm();
+program
+	.command('tool')
+	.description('display template prompt')
+	.action(renderForm);
+  
+if (!process.argv.slice(2).length) {
+	renderForm();
+}
+
+program.parse(process.argv);
+
+/**
+ * render inquirer form
+ */
+function renderForm() {
+	var logo = [
+		'\n',
+		'=================================\n',
+		'        MICROSCOPE-MOBILE        \n',
+		'                                 \n',
+		'    awesome mobile generator     \n',
+		'================================='
+	].join('');
+	console.log(logo);
+	
+	// run repository form
+	new HomeForm();
+}
